572e1eed4f08450239e057e9564aed593fbfbe6b
Paul K <spkim0921@gmail.com>
2023-12-17 23:13:01 -0800

    25th commit
    implemented maintenance mode

--- client/src/router.jsx
+++ client/src/router.jsx
@@ -22,10 +22,10 @@ export function Router() {
         createRoutesFromElements(
             <Route element={<Layout />}>
                 <Route path="/maintenance" element={<HomePage />} loader={pageLoader} />
-                <Route path="/maintenance" element={<PostsPage />} loader={postsLoader} />
+                <Route path="/maintenance2" element={<PostsPage />} loader={postsLoader} />
                 <Route path="/posts/:postId" element={<PostDetailsPage />} loader={postDetailsLoader} />
-                <Route path="/users/maintenance" element={<LoginPage />} />
-                <Route path="/users/maintenance" element={<SignupPage />} />
+                <Route path="/users/maintenance3" element={<LoginPage />} />
+                <Route path="/users/maintenance4" element={<SignupPage />} />
                 <Route path="/users/:userId" element={<ProfilePage />} loader={userPostsLoader} />
                 <Route path="/posts/create" element={<CreatePostPage />} />
                 <Route path="/" element={<MaintenancePage />} />
