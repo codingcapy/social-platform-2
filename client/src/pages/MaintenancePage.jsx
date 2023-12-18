ba6589eed575c870fc2b16f59a49ae5c67504d64
Paul <spkim0921@gmail.com>
2023-12-17 17:01:57 -0800

    24th commit
    implement maintenance mode

---
+++ client/src/pages/MaintenancePage.jsx
@@ -0,0 +1,11 @@
+
+
+
+export default function MaintenancePage() {
+
+    return (
+        <div>
+            <h1 className="py-5 text-2xl text-slate-700 font-medium text-center">We are sorry, CocoDog is currently under maintenance T_T Please visit us on December 20, 2023</h1>
+        </div>
+    )
+}
--- client/src/router.jsx
+++ client/src/router.jsx
@@ -15,18 +15,23 @@ import PostsPage, { postsLoader } from "./pages/PostsPage";
 import ProfilePage, { userPostsLoader } from "./pages/ProfilePage";
 import CreatePostPage from "./pages/CreatePostPage";
 import PostDetailsPage, { postDetailsLoader } from "./pages/PostDetailsPage";
+import MaintenancePage from "./pages/MaintenancePage";
 
 export function Router() {
     const router = createBrowserRouter(
         createRoutesFromElements(
             <Route element={<Layout />}>
-                <Route path="/" element={<HomePage />} loader={pageLoader} />
-                <Route path="/posts" element={<PostsPage />} loader={postsLoader} />
+                <Route path="/maintenance" element={<HomePage />} loader={pageLoader} />
+                <Route path="/maintenance" element={<PostsPage />} loader={postsLoader} />
                 <Route path="/posts/:postId" element={<PostDetailsPage />} loader={postDetailsLoader} />
-                <Route path="/users/login" element={<LoginPage />} />
-                <Route path="/users/signup" element={<SignupPage />} />
+                <Route path="/users/maintenance" element={<LoginPage />} />
+                <Route path="/users/maintenance" element={<SignupPage />} />
                 <Route path="/users/:userId" element={<ProfilePage />} loader={userPostsLoader} />
                 <Route path="/posts/create" element={<CreatePostPage />} />
+                <Route path="/" element={<MaintenancePage />} />
+                <Route path="/posts/" element={<MaintenancePage />} />
+                <Route path="/users/login" element={<MaintenancePage />} />
+                <Route path="/users/signup" element={<MaintenancePage />} />
             </Route>
         )
     )
